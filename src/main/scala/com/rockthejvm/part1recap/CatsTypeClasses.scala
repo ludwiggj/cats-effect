add2c2a0896f872bde335808551962312308d7b8
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2022-09-26 22:39:42 +0100

    Part 1 recap completed

---
+++ src/main/scala/com/rockthejvm/part1recap/CatsTypeClasses.scala
@@ -0,0 +1,138 @@
+package com.rockthejvm.part1recap
+
+// - applicative
+// - functor
+// - flatMap
+// - monad
+// - apply
+// - applicativeError / monadError
+// - traverse
+
+object CatsTypeClasses {
+  // (1) functor - "mappable"
+  trait MyFunctor[F[_]] {
+    def map[A, B](initialValue: F[A])(f: A => B): F[B]
+  }
+
+  import cats.Functor
+
+  val listFunctor = Functor[List]
+
+  // generalise mapping APIs
+  def increment[F[_]](container: F[Int])(using functor: Functor[F]):F[Int] =
+    functor.map(container)(_ + 1)
+
+  import cats.syntax.functor.* // map extension method
+
+  def incrementV2[F[_]: Functor](container: F[Int]):F[Int] =
+    container.map(_ + 1)
+
+  // (2) applicative - the ability to wrap types
+  trait MyApplicative[F[_]] extends Functor[F] {
+    def pure[A](v: A): F[A]
+  }
+
+  import cats.Applicative
+
+  val applicativeList = Applicative[List]
+
+  import cats.syntax.applicative.* // pure extension method
+
+  // (3) flatMap - ability to chain multiple wrapper computations
+  trait MyFlatMap[F[_]] extends MyFunctor[F] {
+    def flatMap[A, B](container: F[A])(f: A => F[B]): F[B]
+  }
+
+  import cats.FlatMap
+
+  val flatMapList = FlatMap[List]
+
+  import cats.syntax.flatMap.* // flatMap extension method
+
+  // flatMap isn't used explicitly
+  def crossProductAlt[F[_], A, B](fa: F[A], fb: F[B])(using flatMap: FlatMap[F]) =
+    fa.flatMap(a => fb.map(b => (a, b))) // map available via Flatmap
+
+  def crossProduct[F[_]: FlatMap, A, B](fa: F[A], fb: F[B]) =
+    fa.flatMap(a => fb.map(b => (a, b)))
+
+  // (4) Monad = applicative + flatmap
+  trait MyMonad[F[_]] extends MyFlatMap[F] with MyApplicative[F] {
+    override def map[A, B](initialValue: F[A])(f: A => B): F[B] =
+      flatMap(initialValue)(a => pure(f(a)))
+  }
+
+  import cats.Monad
+
+  val monadList = Monad[List]
+
+  def crossProduct2[F[_]: Monad, A, B](fa: F[A], fb: F[B]) =
+    for {
+      a <- fa
+      b <- fb
+    } yield (a, b)
+
+  // (5) MyApplicativeError
+  trait MyApplicativeError[F[_], E] extends MyApplicative[F] {
+    def raiseError[A](e: E): F[A]
+  }
+
+  import cats.ApplicativeError
+  type ErrorOr[A] = Either[String, A]
+
+  val appErrorEither = ApplicativeError[ErrorOr, String] // Arguments are WrapperType, ErrorType
+
+  val desirable: ErrorOr[Int] = appErrorEither.pure(9)
+  val undesirable: ErrorOr[Int] = appErrorEither.raiseError[Int]("Oh no")
+
+  import cats.syntax.applicativeError.* // raiseError extension method
+
+  // Can be used for any types for which given ApplicativeError is in scope
+  val undesirable2: ErrorOr[Int] = "Nope".raiseError[ErrorOr, Int]
+  val undesirable3: ErrorOr[Double] = "Nope".raiseError[ErrorOr, Double]
+
+  // (6) MyMonadError
+  trait MyMonadError[F[_], E] extends MyApplicativeError[F, E] with MyMonad[F]
+
+  import cats.MonadError
+
+  val monadErrorEither = MonadError[ErrorOr, String] // Arguments are WrapperType, ErrorType
+
+  // (7) traverse
+
+  trait MyTraverse[F[_]] extends MyFunctor[F] {
+    def traverse[G[_], A, B](container: F[A])(f: A => G[B]): G[F[B]]
+  }
+
+
+
+  def main(args: Array[String]): Unit = {
+    println(increment(List(1, 2, 3)))
+    println(incrementV2(Option(4)))
+
+    println(applicativeList.pure(43))
+    println(43.pure[List]) // will wrap 43 (or any other type) into List, as long as Applicative[List] is in scope
+
+    println(crossProduct(List(1, 2), List(3, 4, 5)))
+
+    println(crossProduct2(List(1, 2), List(3, 4, 5)))
+
+    println(desirable)
+    println(undesirable)
+    println(undesirable2)
+    println(undesirable3)
+
+    // traverse use case - turn nested wrappers inside out
+    val listOfOptions = List(Some(1), Some(2), Some(3))
+
+    import cats.Traverse
+    val listTraverse = Traverse[List]
+    val optionList = listTraverse.traverse(List(1, 2, 3))(Option.apply)
+    println(optionList)
+
+    import cats.syntax.traverse.*
+
+    val optionList2 = List(1, 2, 3).traverse(Option.apply)
+    println(optionList2)
+  }
+}
--- src/main/scala/com/rockthejvm/part1recap/TypeClassesScala3.scala
+++ src/main/scala/com/rockthejvm/part1recap/TypeClassesScala3.scala
@@ -1,6 +1,5 @@
 package com.rockthejvm.part1recap
 
-// 16:47
 object TypeClassesScala3 {
   // Typeclasses specify an interface that attaches behaviour to some types that you want to support the behaviour,
   // but not others
@@ -11,46 +10,44 @@ object TypeClassesScala3 {
   }
 
   // (2) The typeclass instances - specifies which types support the interface specified in part (1)
-  //     val flavour
-  implicit val stringSerializer: JSONSerializer[String] = (s: String) => s"\"$s\""
+  given stringSerializer: JSONSerializer[String] = (s: String) => s"\"$s\""
 
-  implicit val intSerializer: JSONSerializer[Int] = (i: Int) => i.toString
+  given intSerializer: JSONSerializer[Int] with {
+    override def toJson(i: Int): String = i.toString
+  }
 
-  //     object flavour
   case class Person(name: String, age: Int)
 
   import JSONSyntax.*
 
-  implicit object PersonSerializer extends JSONSerializer[Person] {
+  given personSerializer: JSONSerializer[Person] with {
     override def toJson(person: Person): String =
       s"""{"name": ${person.name.toJson}, "age": ${person.age.toJson}}"""
   }
 
   // (2.1) Utility methods
-  implicit def listSerializer[T](implicit serializer: JSONSerializer[T]): JSONSerializer[List[T]] =
+  given listSerializer[T](using serializer: JSONSerializer[T]): JSONSerializer[List[T]] =
     (values: List[T]) => values.map(serializer.toJson).mkString("[", ",", "]")
 
   // (3) Offer the API
-  def serializeToJson[T](value: T)(implicit serializer: JSONSerializer[T]): String =
+  def serializeToJson[T](value: T)(using serializer: JSONSerializer[T]): String =
     serializer.toJson(value)
 
-  def serializeListToJson[T](list: List[T])(implicit serializer: JSONSerializer[T]): String =
+  def serializeListToJson[T](list: List[T])(using serializer: JSONSerializer[T]): String =
     list.map(serializer.toJson).mkString("[", ",", "]")
 
   // (4) Add extension method
   object JSONSyntax {
-    implicit class JsonSerializable[T](value: T)(implicit serializer: JSONSerializer[T]) {
-      def toJson: String = serializer.toJson(value)
+    extension[T](value: T) {
+      def toJson(using serializer: JSONSerializer[T]): String = serializer.toJson(value)
     }
   }
 
   def main(args: Array[String]): Unit = {
-    val mrG = Person("Graeme", 52)
-    val mrsH = Person("Audrey", 56)
+    val mrG = Person("Mr Graeme", 52)
+    val mrsH = Person("Mrs Audrey", 56)
 
     val peeps = List(mrG, mrsH)
-
-    println(JsonSerializable(mrG).toJson)
 
     println(serializeToJson(mrG))
     println(serializeListToJson(peeps))
